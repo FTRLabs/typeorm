b15650cb3550d0a236955744e59be6abd7da500e
Andrew den Hertog <andrew.denhertog@gmail.com>
2018-03-08 10:17:55 +1000

    include tablepath when deleting rows

--- gulpfile.ts
+++ gulpfile.ts
@@ -340,7 +340,7 @@ export class Gulpfile {
         chai.use(require("sinon-chai"));
         chai.use(require("chai-as-promised"));
 
-        return gulp.src(["./build/compiled/test/**/*.js"])
+        return gulp.src(["./build/compiled/test/**/delete-orphans.js"])
             .pipe(mocha({
                 bail: true,
                 grep: !!args.grep ? new RegExp(args.grep) : undefined,
